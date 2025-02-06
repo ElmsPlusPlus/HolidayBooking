203c9664bf35a71e28ffde388ddde47f4417a0ea
Alexander.Elms <alexander.elms@crowddynamics.com>
2025-02-06 23:33:34 +0000

    [+] First passing tests for deserializing data.

--- HolidayBooking.Tests/HolidayBookingTests.cs
+++ HolidayBooking.Tests/HolidayBookingTests.cs
@@ -1,11 +1,32 @@
+using Newtonsoft.Json;
+using FluentAssertions;
+
 namespace HolidayBooking.Tests
 {
     public class HolidayBookingTests
     {
         [Fact]
-        public void Test1()
+        public void DeserializeFlightDataJSONTest()
         {
+            FlightData? flightData = JsonConvert.DeserializeObject<FlightData>("{}");
 
+            flightData.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void DeserializeHotelDataJSONTest()
+        {
+            HotelData? hotelData = JsonConvert.DeserializeObject<HotelData>("{}");
+
+            hotelData.Should().NotBeNull();
+        }
+
+        private class FlightData
+        {
+        }
+
+        private class HotelData
+        {
         }
     }
 }
