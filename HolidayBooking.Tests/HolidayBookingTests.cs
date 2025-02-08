d224f337876c99fc9b466697513a0952d6d63f4d
Alexander.Elms <alexander.elms@crowddynamics.com>
2025-02-08 18:13:57 +0000

    [+] Added tests for all remaining flight parameters. [!] Correct test not checking type of integer for id.

--- HolidayBooking.Tests/HolidayBookingTests.cs
+++ HolidayBooking.Tests/HolidayBookingTests.cs
@@ -7,13 +7,25 @@ namespace HolidayBooking.Tests
 {
     public class HolidayBookingTests
     {
-        string flightDataTestJSON = @"{
-                            'id': 1,
-                            }";
+        static string flightDataTestJSON = @"{
+                                    'id': 1,
+                                    'airline': ""First Class Air"",
+                                    'from': ""MAN"",
+                                    'to': ""TFS"",
+                                    'price': 470,
+                                    'departure_date': ""2023-07-01""
+                                    }";
 
         static string hotelDataTestJSON = @"{
-                            'id': 1,
-                            }";
+                                            'id': 1,
+                                            'name': ""Iberostar Grand Portals Nous"",
+                                            'arrival_date': ""2022-11-05"",
+                                            'price_per_night': 100,
+                                            'local_airports': [ ""TFS"" ],
+                                            'nights': 7
+                                            }";
+
+
 
         [Fact]
         public void DeserializeFlightDataJSONTest()
@@ -36,7 +48,48 @@ namespace HolidayBooking.Tests
         {
             FlightData flightData = JsonConvert.DeserializeObject<FlightData>(flightDataTestJSON);
 
-            flightData.id.Should().Be(1);
+            //flightData.id.Should().BeOfType<int>();
+            Assert.IsType<int>(flightData.id);
+        }
+
+        [Fact]
+        public void FlightDataAirlineIsAString()
+        {
+            FlightData flightData = JsonConvert.DeserializeObject<FlightData>(flightDataTestJSON);
+
+            flightData.airline.Should().BeOfType<string>();
+        }
+
+        [Fact]
+        public void FlightDataFromIsAString()
+        {
+            FlightData flightData = JsonConvert.DeserializeObject<FlightData>(flightDataTestJSON);
+
+            flightData.from.Should().BeOfType<string>();
+        }
+
+        [Fact]
+        public void FlightDataToIsAString()
+        {
+            FlightData flightData = JsonConvert.DeserializeObject<FlightData>(flightDataTestJSON);
+
+            flightData.to.Should().BeOfType<string>();
+        }
+
+        [Fact]
+        public void FlightDataPriceIsAnInteger()
+        {
+            FlightData flightData = JsonConvert.DeserializeObject<FlightData>(flightDataTestJSON);
+
+            Assert.IsType<int>(flightData.price);
+        }
+
+        [Fact]
+        public void FlightDataDepartureDataIsAString()
+        {
+            FlightData flightData = JsonConvert.DeserializeObject<FlightData>(flightDataTestJSON);
+
+            flightData.departure_date.Should().BeOfType<string>();
         }
 
         [Fact]
@@ -44,12 +97,18 @@ namespace HolidayBooking.Tests
         {
             HotelData hotelData = JsonConvert.DeserializeObject<HotelData>(hotelDataTestJSON);
 
-            hotelData.id.Should().Be(1);
+            //hotelData.id.Should().Be(typeof(int));
+            Assert.IsType<int>(hotelData.id);
         }
 
         private class FlightData
         {
             public int id { get; set; }
+            public string airline { get; set; }
+            public string from { get; set; }
+            public string to { get; set; }
+            public int price { get; set; }
+            public string departure_date { get; set; }
         }
 
         private class HotelData
