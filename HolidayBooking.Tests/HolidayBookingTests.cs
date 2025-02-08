c314141294bd0e7cc6509dd912148d81630549c2
Alexander.Elms <alexander.elms@crowddynamics.com>
2025-02-08 12:02:01 +0000

    [+] Added test to confirm ID data is an integer.

--- HolidayBooking.Tests/HolidayBookingTests.cs
+++ HolidayBooking.Tests/HolidayBookingTests.cs
@@ -1,14 +1,24 @@
 using Newtonsoft.Json;
 using FluentAssertions;
+using Xunit.Sdk;
+using System.Runtime.Serialization;
 
 namespace HolidayBooking.Tests
 {
     public class HolidayBookingTests
     {
+        string flightDataTestJSON = @"{
+                            'id': 1,
+                            }";
+
+        static string hotelDataTestJSON = @"{
+                            'id': 1,
+                            }";
+
         [Fact]
         public void DeserializeFlightDataJSONTest()
         {
-            FlightData? flightData = JsonConvert.DeserializeObject<FlightData>("{}");
+            FlightData flightData = JsonConvert.DeserializeObject<FlightData>(flightDataTestJSON);
 
             flightData.Should().NotBeNull();
         }
@@ -16,17 +26,35 @@ namespace HolidayBooking.Tests
         [Fact]
         public void DeserializeHotelDataJSONTest()
         {
-            HotelData? hotelData = JsonConvert.DeserializeObject<HotelData>("{}");
+            HotelData hotelData = JsonConvert.DeserializeObject<HotelData>(hotelDataTestJSON);
 
             hotelData.Should().NotBeNull();
         }
 
+        [Fact]
+        public void FlightDataIDIsAnInteger()
+        {
+            FlightData flightData = JsonConvert.DeserializeObject<FlightData>(flightDataTestJSON);
+
+            flightData.id.Should().Be(1);
+        }
+
+        [Fact]
+        public void HotelDataIDIsAnInteger()
+        {
+            HotelData hotelData = JsonConvert.DeserializeObject<HotelData>(hotelDataTestJSON);
+
+            hotelData.id.Should().Be(1);
+        }
+
         private class FlightData
         {
+            public int id { get; set; }
         }
 
         private class HotelData
-        {
+        { 
+            public int id { get; set; }
         }
     }
 }
