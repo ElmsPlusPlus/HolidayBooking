2dc55267f24c241f78e20c27fb931773eeb773b9
Alexander.Elms <alexander.elms@crowddynamics.com>
2025-02-08 23:29:33 +0000

    [+] Initial Creation of Holiday Search class.

---
+++ HolidayBooking/HolidaySearch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HolidayBooking.Tests;
+using Newtonsoft.Json;
+
+namespace HolidayBooking
+{
+    public class HolidaySearch
+    {
+        private FlightsData flightsData;
+        private HotelsData hotelsData;
+
+        public HolidaySearch(string flightsDataFile, string hotelsDataFile)
+        {
+            flightsData = JsonConvert.DeserializeObject<FlightsData>(flightsDataFile);
+
+            hotelsData = JsonConvert.DeserializeObject<HotelsData>(hotelsDataFile);
+        }
+    }
+}
